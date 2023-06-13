f16bf61f33a08d9d3b5e6d451aef267802d52fd8
jdpulido654 <127062654+jdpulido654@users.noreply.github.com>
2023-06-13 09:25:07 -0500

    Update TestUserRepositoryImpl.java

--- project_2687365/src/main/java/model/repository/TestUserRepositoryImpl.java
+++ project_2687365/src/main/java/model/repository/TestUserRepositoryImpl.java
@@ -1,27 +1,27 @@
-package co.edu.sena.project_2687365.model.repository;
+package co.edu.sena.project_2687365.repository;
 
-import co.edu.sena.project_2687365.model.beans.User;
+import model.User;
 
 import java.sql.SQLException;
 
 
 public class TestUserRepositoryImpl {
     public static void main(String[] args) throws SQLException {
-        co.edu.sena.project_2687365.model.repository.Repository<User> repository = new UserRepositoryImpl();
+        Repository<User> repository = new UserRepositoryImpl();
 
         System.out.println("========saveObj Insert===========");
         User userInsert1 = new User();
-        userInsert1.setUser_firstname("jose");
-        userInsert1.setUser_lastname("perez");
-        userInsert1.setUser_email("perez@gmail.com");
-        userInsert1.setUser_password("jose12");
+        userInsert1.setUser_fistname("Jairo");
+        userInsert1.setUser_lastname("Hernandez");
+        userInsert1.setUser_email("jairo14@mail.com");
+        userInsert1.setUser_password("jairo12");
         repository.saveObj(userInsert1);
 
         User userInsert2 = new User();
-        userInsert2.setUser_firstname("michel");
-        userInsert2.setUser_lastname("laiton");
-        userInsert2.setUser_email("michel12@gmail.com");
-        userInsert2.setUser_password("michell23");
+        userInsert2.setUser_fistname("Camilo");
+        userInsert2.setUser_lastname("Hernandez");
+        userInsert2.setUser_email("camilo18@mail.com");
+        userInsert2.setUser_password("camilo142");
         repository.saveObj(userInsert2);
 
 
@@ -35,10 +35,10 @@ public class TestUserRepositoryImpl {
 
         System.out.println("============== saveObj ===========");
         User userUpdate = new User();
-        userUpdate.setUser_firstname("Fabian");
-        userUpdate.setUser_lastname("lopez");
-        userUpdate.setUser_email("fabian12@gmail.com");
-        userUpdate.setUser_password("fabiana12");
+        userUpdate.setUser_fistname("Andrea");
+        userUpdate.setUser_lastname("Ramos");
+        userUpdate.setUser_email("Randrea12@mail.com");
+        userUpdate.setUser_password("andrea12");
         repository.listAllObj().forEach(System.out::println);
         System.out.println();
 
@@ -48,4 +48,3 @@ public class TestUserRepositoryImpl {
 
     }
 }
-
